2404b4226e22e17d7859aca80d9ef8f4cab20ea0
kristofer15 <kristofer15@ru.is>
2017-01-18 21:04:03 +0000

    protoclass yfir í venjulegan class

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -1,3 +1,4 @@
+//import shape from "shape.js"
 var settings = {
   canvas: undefined,    // Gets set when the document is ready
   nextOjbect: "line",
@@ -5,38 +6,56 @@ var settings = {
   nextShape: undefined  // Gets set when mousedown initiates a new shape
 };
 
+
 $(document).ready(function () {
 
   settings.canvas = document.getElementById("myCanvas");        // Get the canvas object now that document is ready
 
   $(settings.canvas).mousedown(function(e) {
-    settings.nextShape = new protoShape(e.clientX, e.clientY);  // start a new shape
+    settings.nextShape = new Line(e.clientX, e.clientY);  // start a new shape
   });
 
   $(settings.canvas).mouseup(function(e) {
     var shape = settings.nextShape;
 
     if(shape !== undefined) { // mousedown has started a new shape
+      var context = settings.canvas.getContext("2d");
       shape.endX = e.clientX;
       shape.endY = e.clientY;
-      drawLine(shape);
+      shape.draw(context);
     }
   })
 });
 
-function drawLine(line) {
-  context = settings.canvas.getContext("2d");
-  context.beginPath();                      // Start a line shape
-  context.moveTo(line.startX, line.startY); // Designate starting point
-  context.lineTo(line.endX, line.endY);     // Designate endpoint
-  context.stroke();                         // Draw the line
 
-  settings.nextShape = undefined;           // Line is drawn, no longer in progress
+
+//ES6 classes
+//TODO: find a way to move them to their own files. Import isn't working.
+class Shape {
+  constructor(x, y) {
+    this.startX = x;
+    this.startY = y;
+    this.endX = undefined;
+    this.endY = undefined;
+  }
+
+  setEnd(x, y) {
+    this.endX = x;
+    this.endY = y;
+  }
 }
 
-// Prototype shape
-// Replace with standalone classes
-function protoShape(x, y) {
-  this.startX = x;
-  this.startY = y;
-};
+class Line extends Shape {
+  constructor(x, y) {
+    super(x, y);
+  }
+
+  draw(context) {
+    context.beginPath();                      // Start a line shape
+    context.moveTo(this.startX, this.startY); // Designate starting point
+    context.lineTo(this.endX, this.endY);     // Designate endpoint
+    context.stroke();                         // Draw the line
+
+    settings.nextShape = undefined;           // Line is drawn, no longer in progress
+  }
+}
