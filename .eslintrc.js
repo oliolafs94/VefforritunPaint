bc13edbceeeef508812301050494d4d66bb1958b
Kristofer Andersen <kristofer15@ru.is>
2017-01-20 21:24:29 +0000

    double quotes mandatory í eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "extends": "eslint:recommended",
     "rules": {
-      "no-console": "off"
+      "no-console": "off",
+      "quotes": ["error", "double"]
     },
     "env": {
       "es6": true
