eee4950789f5abde66340d364057d1afa4c7fc1c
Kristofer Andersen <kristofer15@ru.is>
2017-01-23 14:21:53 +0000

    eslint reglur

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "extends": "eslint:recommended",
     "rules": {
+      "spaced-comment": ["error", "always"],
+      "no-use-before-define": "error",
+      "no-multiple-empty-lines": "error",
+      "semi": ["error", "always"],
+      "no-unreachable": "error",
       "no-console": "off",
       "quotes": ["error", "double"]
     },
